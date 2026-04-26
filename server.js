c8689d2c2c7bd6130d1148e88b64ca8825824ff1
Danny3819 <77942271+danny3819@users.noreply.github.com>
2026-04-25 20:42:58 -0600

    trust agregado

--- server.js
+++ server.js
@@ -8,6 +8,7 @@ require("dotenv").config();
 const app = express();
 const port = process.env.PORT || 3000;
 
+app.set("trust proxy", 1);
 app.set("view engine", "ejs");
 app.use(express.static("public"));
 app.use(express.urlencoded({ extended: true }));
